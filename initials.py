be8187c0044e543cad1e8f78f428fb56d47cd9eb
Seyram22 <ankugahelemawusinuseyram@gmail.com>
2026-05-29 20:50:03 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Seyram Elamawusinu Ankugah
+# STUDENT ID: 2425403050
+# Email: ankugahelemawusinuseyram@gmail.com
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor, Seyram Elamawusinu Ankugah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This main function prints my initials in small and large stylized letters.
+    print()
+    print("...SEA")
+    print()
 
+    print("...SSSSSSSSSSSS........EEEEEEEEEEEE............AAAA.........")
+    print("...SSSSSSSSSSSS........EEEEEEEEEEEE...........AAAAAA........")
+    print("...SSSS................EEEE..................AAA..AAA.......")
+    print("...SSSS................EEEE.................AAA....AAA......")
+    print("....SSSSSS.............EEEEEEEEEE...........AAA....AAA......")
+    print("......SSSSSS...........EEEEEEEEEE...........AAAAAAAAAA......")
+    print(".........SSSS..........EEEE.................AAAAAAAAAA......")
+    print("...........SSSS........EEEE.................AAA....AAA......")
+    print("...SSSSSSSSSSSS...**...EEEEEEEEEEEE...**....AAA....AAA....**")
+    print("...SSSSSSSSSSSS...**...EEEEEEEEEEEE...**....AAA....AAA....**")
+    print()
 
 
 main()
